2b60035684a57a1ef95a9328275eef46ad5e579e
Venomalia <Venomalia@mailbox.org>
2021-09-12 14:21:30 +0200

    checks for missing images and reassigns them.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace DolphinDynamicInputTextureCreator
         private void SetInputPack(Data.DynamicInputPack pack)
         {
             DataContext = pack;
+            TestInputPack(InputPack);
             ((ViewModels.PanZoomViewModel)PanZoom.DataContext).InputPack = pack;
         }
 
@@ -219,5 +220,47 @@ namespace DolphinDynamicInputTextureCreator
                 _edit_metadata_window.Content = user_control;
             }
         }
+
+        #region Test
+        public void TestInputPack(Data.DynamicInputPack inputPack)
+        {
+            foreach (Data.HostDevice device in inputPack.HostDevices)
+            {
+                foreach (Data.HostKey key in device.HostKeys)
+                {
+                    if (!File.Exists(key.TexturePath))
+                    {
+                        key.TexturePath = GetNewImage(key.Name, key.TexturePath);
+                    }
+                }
+            }
+            foreach (Data.DynamicInputTexture texture in inputPack.Textures)
+            {
+                if (!File.Exists(texture.TexturePath))
+                {
+                    texture.TexturePath = GetNewImage(texture.TextureHash, texture.TexturePath);
+                }
+            }
+        }
+
+        private string GetNewImage(string name, string paht)
+        {
+            MessageBoxResult MessageResult;
+            MessageResult = MessageBox.Show(String.Format("The image of '{0}' could not be found!\n'{1}'\nsearch for the picture?", name, paht), name, MessageBoxButton.YesNo);
+            if (MessageResult == MessageBoxResult.Yes)
+            {
+                var dialog = new System.Windows.Forms.OpenFileDialog();
+                dialog.DefaultExt = ".png";
+                dialog.Filter = "PNG Files (*.png)|*.png";
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    return dialog.FileName;
+                }
+            }
+            return paht;
+        }
+        #endregion
+
     }
 }
