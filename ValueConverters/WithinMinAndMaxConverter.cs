d1bb6f1bded56eb686932d00a8f592b5ca6d185a
Venomalia <Venomalia@mailbox.org>
2022-02-17 09:49:17 +0100

    min max

---
+++ ValueConverters/WithinMinAndMaxConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace DolphinDynamicInputTextureCreator.ValueConverters
+{
+
+    [ValueConversion(typeof(int), typeof(int))]
+    public class IntWithinMinAndMaxConverter : WithinMinAndMaxConverter<int> {}
+
+    public class WithinMinAndMaxConverter<T> : IValueConverter where T : IComparable<T>, IConvertible
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Within((T)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return Within((T)System.Convert.ChangeType(value, typeof(T)));
+            }
+            catch(Exception)
+            {
+                return Min;
+            }
+        }
+
+        private T Within(T value)
+        {
+            if (value.CompareTo(Min) < 0)
+            {
+                return Min;
+            }
+            if (value.CompareTo(Max) > 0)
+            {
+                return Max;
+            }
+            return value;
+        }
+
+    }
+}
